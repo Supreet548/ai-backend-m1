9c36e2e50b6575e0a246f8a27d4f4d5b9264ee22
Supreet Kumar <supreetkumar548@gmail.com>
2026-04-30 17:13:59 +0530

    FastAPI with PostgreSQL

--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,40 @@
 from fastapi import APIRouter
 from .schemas import UserSchema
 from .utils import UserHelper
-import asyncio
+from .database import get_connection
 
 router = APIRouter()
 
-users = []
-@router.get("/")
-async def root():
-    return {"message": "Async API Running"}
-
-@router.get("/about")
-async def about():
-    return{
-        "project": "AI Backend M1"
-    }
-
-
-
 @router.post("/users")
-async def create_user(user:UserSchema):
-    users.append(user.model_dump())
+def create_user(user:UserSchema):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "Insert INTO users (name, email, age, city) VALUES(%s, %s, %s, %s)",
+        (user.name, user.email, user.age, user.city)
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
     helper = UserHelper(user.name)
-    
 
-    return{
+    return {
         "success": True,
-        "message": helper.welcome(),
-        "role": helper.role(),
-        "data": user
+        "message": helper.welcome()
     }
 
-
 @router.get("/users")
-async def get_users():
-    return users
+def get_users():
+    conn = get_connection()
+    cur = conn.cursor()
 
-@router.get("/slow")
-async def slow_api():
-    await asyncio.sleep(5)
-    return {"message":"Finished after 5 seconds"}
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
 
-@router.get("/wait2")
-async def wait_api():
-    await asyncio.sleep(2)
-    return{"message":"Finished after 2 seconds"}
+    cur.close()
+    conn.close()
 
-
-@router.get("/hello")
-async def hello_api():
-    return{"message":"Hello Async World"}
+    return rows
