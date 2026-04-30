e1ebe017963b094f4f1add7dd7ad589f14a37546
Supreet Kumar <supreetkumar548@gmail.com>
2026-04-30 14:55:50 +0530

    Added async routes and await

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter
 from .schemas import UserSchema
 from .utils import UserHelper
+import asyncio
 
 router = APIRouter()
 
 users = []
+@router.get("/")
+async def root():
+    return {"message": "Async API Running"}
+
+@router.get("/about")
+async def about():
+    return{
+        "project": "AI Backend M1"
+    }
+
+
 
 @router.post("/users")
-def create_user(user:UserSchema):
+async def create_user(user:UserSchema):
     users.append(user.model_dump())
 
     helper = UserHelper(user.name)
@@ -22,5 +34,20 @@ def create_user(user:UserSchema):
 
 
 @router.get("/users")
-def get_users():
+async def get_users():
     return users
+
+@router.get("/slow")
+async def slow_api():
+    await asyncio.sleep(5)
+    return {"message":"Finished after 5 seconds"}
+
+@router.get("/wait2")
+async def wait_api():
+    await asyncio.sleep(2)
+    return{"message":"Finished after 2 seconds"}
+
+
+@router.get("/hello")
+async def hello_api():
+    return{"message":"Hello Async World"}
