0927a99a9a2398d67c963d853bdbe88a6b6aba2d
Supreet Kumar <supreetkumar548@gmail.com>
2026-05-05 18:04:35 +0530

    Middleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from app.routes import user_routes
+from contextlib import asynccontextmanager
 from app.database.async_connection import create_pool
+from app.middleware.logging_middleware import LoggingMiddleware
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
     await create_pool()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.add_middleware(LoggingMiddleware)
 
 app.include_router(user_routes.router)
 
---
+++ app/middleware/logging_middleware.py
@@ -0,0 +1,31 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi import Request
+import time
+import uuid
+from app.logger import logger
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+
+    async def dispatch(self, request: Request, call_next):
+
+        # 🔹 Step 3: Generate Request ID
+        request_id = str(uuid.uuid4())
+
+        start_time = time.time()
+
+        logger.info(f"[{request_id}] Incoming request: {request.method} {request.url}")
+
+        response = await call_next(request)
+
+        process_time = time.time() - start_time
+
+        logger.info(
+            f"[{request_id}] Completed response: {response.status_code} in {process_time:.4f}s"
+        )
+
+        # 🔹 Step 4: Add headers to response
+        response.headers["X-Process-Time"] = str(process_time)
+        response.headers["X-Request-ID"] = request_id
+
+        return response
